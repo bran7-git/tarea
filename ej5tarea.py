57859de680713433d68e22ec2e45a2ba68e6f85a
bran7-git <brandoncasse98@gmail.com>
2025-05-26 21:50:28 -0300

    Update ej5tarea.py

--- ej5tarea.py
+++ ej5tarea.py
@@ -1,16 +1,19 @@
 def romano_a_decimal(romano):
-    valores = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+    valores = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                'C': 100, 'D': 500, 'M': 1000}
 
-    if len(romano) == 0:
-        return 0
     if len(romano) == 1:
         return valores[romano]
-
-    if valores[romano[0]] < valores[romano[1]]:
-        return valores[romano[1]] - valores[romano[0]] + romano_a_decimal(romano[2:])
     else:
-        return valores[romano[0]] + romano_a_decimal(romano[1:])
+        if valores[romano[0]] >= valores[romano[1]]:
+            return valores[romano[0]] + romano_a_decimal(romano[1:])
+        else:
+            return romano_a_decimal(romano[1:]) - valores[romano[0]]
 
 
-print(romano_a_decimal('MCMXCIV'))  
+# Ejemplos de prueba
+print(romano_a_decimal("III"))       # 3
+print(romano_a_decimal("IV"))        # 4
+print(romano_a_decimal("IX"))        # 9
+print(romano_a_decimal("LVIII"))     # 58
+print(romano_a_decimal("MCMXCIV"))   # 1994
